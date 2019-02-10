a813f588e499848daf48d3724a6556cba05fd35a
Otto-Ville Savolainen <>
2019-02-10 12:58:54 +0200

    Remove AsyncStorage usage from LoadingScreen

--- screens/LoadingScreen.js
+++ screens/LoadingScreen.js
@@ -1,26 +1,11 @@
 import React from 'react';
 import { Text, StyleSheet, View } from 'react-native';
-import {AsyncStorage} from 'react-native';
 
 export default class LoadingScreen extends React.Component {
 
   constructor() {
     super();
-    this._retrieveData();
   }
-
-  _retrieveData = async () => {
-    try {
-      const value = await AsyncStorage.getItem('favoriteCampus');
-      if (value !== null) {
-        console.log(value);
-        this.props.navigation.navigate(value);
-      } else {
-        this.props.navigation.navigate('Keskusta');
-      }
-    } catch (error) {
-    }
-  };
 
   render() {
     return (
