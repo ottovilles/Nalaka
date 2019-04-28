cebee4b31c5f774e8cb61dca52883060a7e3eb72
Otto-Ville Savolainen <>
2019-04-28 11:14:53 +0300

    Improve loading screen

--- screens/LoadingScreen.js
+++ screens/LoadingScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Text, StyleSheet, View } from 'react-native';
-
+import { PacmanIndicator } from 'react-native-indicators';
 export default class LoadingScreen extends React.Component {
 
   constructor() {
@@ -10,7 +10,8 @@ export default class LoadingScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Loading...</Text>
+        <PacmanIndicator style={styles.indicator} color='orange' size={80} />
+        <Text style={styles.loadingText}>Käynnistetään sovellusta...</Text>
       </View>
     );
   }
@@ -18,9 +19,19 @@ export default class LoadingScreen extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    display: 'flex',
     backgroundColor: '#fff',
+    flexDirection: 'column',
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    flex: 1
   },
+  indicator: {
+    position: 'absolute'
+  },
+  loadingText: {
+    marginBottom: '-25%',
+    fontSize: 20,
+    fontWeight: 'bold'
+  }
 });
